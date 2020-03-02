2496099c00ddbc6e1fae9fcad2efe3623d371123
SOUTHYYY <leshka.l1337@gmail.com>
2020-03-02 09:50:04 +0300

    Update App.js / Delete trash / Icons in progress

--- App.js
+++ App.js
@@ -8,51 +8,14 @@
 
 import React from 'react';
 import { Button, Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-//import all the basic component we have used
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Ionicons} from 'react-native-vector-icons/Ionicons';
 //import Ionicons to show the icon for bottom options
 
 
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import {createStackNavigator} from 'react-navigation-stack';
 
 import {MapScreen, TimetableScreen} from "./screens";
-
-// const HomeStack = createStackNavigator(
-//     {
-//       //Defination of Navigaton from home screen
-//     },
-//     {
-//       defaultNavigationOptions: {
-//         //Header customization of the perticular Screen
-//         headerStyle: {
-//           backgroundColor: '#42f44b',
-//         },
-//         headerTintColor: '#FFFFFF',
-//         title: 'Home',
-//         //Header title
-//       },
-//     }
-// );
-// const SettingsStack = createStackNavigator(
-//     {
-//       //Defination of Navigaton from setting screen
-//       Map: { screen: MapScreen },
-//       Timetable: {screen: TimetableScreen}
-//     },
-//     {
-//       defaultNavigationOptions: {
-//         //Header customization of the perticular Screen
-//         headerStyle: {
-//           backgroundColor: '#42f44b',
-//         },
-//         headerTintColor: '#FFFFFF',
-//         title: 'Settings',
-//         //Header title
-//       },
-//     }
-// );
 const App = createBottomTabNavigator(
     {
       Map: { screen: MapScreen },
@@ -69,12 +32,11 @@ const App = createBottomTabNavigator(
           } else if (routeName === 'Timetable') {
             iconName = `ios-checkmark-circle${focused ? '' : '-outline'}`;
           }
-          return <IconComponent name={iconName} size={25} color={tintColor} />;
         },
       }),
       tabBarOptions: {
-        activeTintColor: '#42f44b',
-        inactiveTintColor: 'gray',
+        activeTintColor: '#E61313',
+        inactiveTintColor: '#000',
       },
     }
 );
